27214dd3f039ac612f80f1f28f8530a15b04d7eb
BakaWang <wa11579@hotmail.com>
2017-05-12 10:44:30 +0800

    ignore file

--- practice.js
+++ practice.js
@@ -8,4 +8,6 @@ let test2 = moment('2017-05-11T17:16:15.286+08:00');
 console.log(test);
 console.log(test.toDate());
 console.log(test2);
-console.log(test2.endOf('d').format())
+console.log(test2.endOf('d').format());
+
+let webstormpushtest = 1;
